99168834324b17fc8251461f403d740947455c19
pusilvija <silvijapupsaite@gmail.com>
2025-03-30 19:49:43 +0300

    added api start

---
+++ app/routes.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, jsonify, request, Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/queen', methods=['POST'])
+def put_queen():
+    return jsonify(message="")
+
+
+if __name__ == '__main__':
+    app.run()
