420f2260445c520ca9e4f66c89f19872abe06822
pusilvija <silvijapupsaite@gmail.com>
2025-04-02 13:36:29 +0300

    feat: added queen validation

--- app/grid.py
+++ app/grid.py
@@ -1,57 +1,97 @@
 import numpy as np
 
+
+COLOR_STR = 'color'
+QUEEN_STR = 'queen'
+
 COLOR_INDEX = 0
 QUEEN_INDEX = 1
+
 
 class Grid:
     GRID_SIZE = 4
 
-
     def __init__(self):
-        self.grid = np.zeros((self.GRID_SIZE, self.GRID_SIZE), dtype=object)
+        self.grid = np.zeros((self.GRID_SIZE, self.GRID_SIZE), dtype=[(COLOR_STR, 'U1'), (QUEEN_STR, 'i1')])
+            # 'U1' (a Unicode string of length 1).
+            # 'i1' (a signed 8-bit integer).
+
         self.setCustomGrid()
 
-    
     def setCustomGrid(self):
-        self.grid[0, 0] = ('R', 0)
-        self.grid[1, 0] = ('R', 0)
-        self.grid[2, 0] = ('R', 0)
-        self.grid[3, 0] = ('G', 0)
-
-        self.grid[0, 1] = ('R', 0)
-        self.grid[1, 1] = ('Y', 0)
-        self.grid[2, 1] = ('Y', 0)
-        self.grid[3, 1] = ('G', 0)
-
-        self.grid[0, 2] = ('Y', 0)
-        self.grid[1, 2] = ('Y', 0)
-        self.grid[2, 2] = ('Y', 0)
-        self.grid[3, 2] = ('B', 0)
-
-        self.grid[0, 3] = ('B', 0)
-        self.grid[1, 3] = ('B', 0)
-        self.grid[2, 3] = ('B', 0)
-        self.grid[3, 3] = ('B', 0)
+        colors = np.array([
+            ['R', 'R', 'R', 'G'],
+            ['R', 'Y', 'Y', 'G'],
+            ['Y', 'Y', 'Y', 'B'],
+            ['B', 'B', 'B', 'B']
+        ])
+        self.grid[COLOR_STR] = colors
 
     def putQueen(self, x, y):
-        # Place a queen on the grid at the specified coordinates
-        self.grid[x, y] = (self.grid[x, y][QUEEN_INDEX], 1)
+        self.grid[x, y][QUEEN_STR] = 1
+        self._validateGrid(x, y)
 
     def removeQueen(self, x, y):
-        # Place a queen on the grid at the specified coordinates
-        self.grid[x, y] = (self.grid[x, y][QUEEN_INDEX], 0)
+        self.grid[x, y][QUEEN_STR] = 0
+        self._validateGrid(x, y)
 
-    def validateGrid(self):
-        # Validate the position of the queen on the grid
-        #TODO implement logic for validation
+    def _validateGrid(self, x, y):
+        self._validateColor()
+        self._validateCol()
+        self._validateRow()
+        self._validateNeighbors(x, y)
         
-        
-    def hasQueenOnColor(self):
-        return np.sum((self.grid[:, :, COLOR_INDEX] == 'B') & (self.grid[:, :, QUEEN_INDEX] == 1)) > 1
+    def _validateColor(self):
+        distinct_colors = np.unique(self.grid[COLOR_STR])
+        for color in distinct_colors:
+            if np.sum(self.grid[self.grid[COLOR_STR] == color][QUEEN_STR]) > 1:
+                print(f"Invalid grid: Multiple queens of color {color} found.")
+                return False
+        return True
+    
+    def _validateCol(self):
+        col = self.grid[:, 0]
+        if col[QUEEN_STR].sum() > 1:
+            print("Invalid grid: Multiple queens in the same column.")
+            return False
+        return True
+
+    def _validateRow(self):
+        col = self.grid[0, :]
+        if col[QUEEN_STR].sum() > 1:
+            print("Invalid grid: Multiple queens in the same row.")
+            return False
+        return True
+    
+    def _validateNeighbors(self, x, y):
+        neighbors_coords = np.array(self._getNeighborsCoords(x, y))
+        if self.grid[neighbors_coords[:, 0], neighbors_coords[:, 1]][QUEEN_STR].sum() >= 1:
+            print(f"Invalid grid: Multiple queens in the same neighborhood.")
+            return False
+        return True
+
+    def _getNeighborsCoords(self, x, y):
+        """Get the neighbors of a specific coordinate (x, y) in the grid."""
+        directions = [
+            (-1, -1), (-1, 0), (-1, 1),
+            (0, -1),           (0, 1),
+            (1, -1),  (1, 0),  (1, 1)
+        ]
+        return [
+            (x + dx, y + dy)
+            for dx, dy in directions
+            if 0 <= x + dx < self.GRID_SIZE and 0 <= y + dy < self.GRID_SIZE
+        ]
     
     def printGrid(self):
         print(self.grid)
 
+
 if __name__ == '__main__':
     grid = Grid()
-    grid.printGrid()  # Should return True since no queens are in the same row/column/diagonal
+    grid.putQueen(0, 0)
+    grid.putQueen(0, 1)
+    grid.putQueen(1, 0)
+    grid.putQueen(1, 1)
+
+    grid.printGrid()
